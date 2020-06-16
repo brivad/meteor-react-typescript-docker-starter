3a3b1a1d3461a48cec1b6f320b16e588a95896ad
brivad <brivad@gmail.com>
2020-06-16 17:42:52 +0200

    updates

--- tests/main.ts
+++ tests/main.ts
@@ -1,21 +1,21 @@
-import { Meteor } from 'meteor/meteor';
-import assert from 'assert';
+import { Meteor } from 'meteor/meteor'
+import assert from 'assert'
 
-describe('meteor-app', function () {
+describe('meteor-react-typescript-docker-starter', function () {
   it('package.json has correct name', async function () {
-    const { name } = await import('../package.json');
-    assert.strictEqual(name, 'meteor-app');
-  });
+    const { name } = await import('../package.json')
+    assert.strictEqual(name, 'meteor-react-typescript-docker-starter')
+  })
 
   if (Meteor.isClient) {
     it('client is not server', function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
+      assert.strictEqual(Meteor.isServer, false)
+    })
   }
 
   if (Meteor.isServer) {
     it('server is not client', function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
+      assert.strictEqual(Meteor.isClient, false)
+    })
   }
-});
+})
